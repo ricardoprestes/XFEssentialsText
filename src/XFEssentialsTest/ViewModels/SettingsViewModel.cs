15292a321307fcf1ddc5758effde3fa642aed440
Ricardo Prestes <ricardo.logan@hotmail.com>
2020-01-24 10:05:19 -0300

    Adding page and viewmodel

--- src/XFEssentialsTest/ViewModels/SettingsViewModel.cs
+++ src/XFEssentialsTest/ViewModels/SettingsViewModel.cs
@@ -1,10 +1,39 @@
 ﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XFEssentialsTest.Helpers;
+
 namespace XFEssentialsTest.ViewModels
 {
     public class SettingsViewModel : BaseViewModel
     {
+        string _token;
+        public string Token
+        {
+            get => _token;
+            set => SetProperty(ref _token, value);
+        }
+
+        public ICommand SaveCommand { get; private set; }
+        public ICommand ResetCommand { get; private set; }
+
         public SettingsViewModel()
         {
+            Token = Settings.Token;
+
+            SaveCommand = new Command(SaveExecute);
+            ResetCommand = new Command(ResetExecute);
+        }
+
+        private void SaveExecute()
+        {
+            Settings.Token = Token;
+        }
+
+        private void ResetExecute()
+        {
+            Settings.Reset();
+            Token = null;
         }
     }
 }
