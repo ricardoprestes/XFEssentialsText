3d3367b39b99040cc2af5fee173c983b8576b556
Ricardo Prestes <ricardo.logan@hotmail.com>
2020-01-24 08:55:34 -0300

    Creating settings class

---
+++ src/XFEssentialsTest/Helpers/Settings.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Essentials;
+
+namespace XFEssentialsTest.Helpers
+{
+    public static class Settings
+    {
+        public static string Token
+        {
+            get => SecureStorage.GetAsync(nameof(Token)).Result;
+            set => SecureStorage.SetAsync(nameof(Token), value);
+        }
+
+        public static void Reset()
+        {
+            SecureStorage.RemoveAll();
+        }
+    }
+}
